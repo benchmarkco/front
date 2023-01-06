fe6cae16da97e447a56e3dd65f722f053bf7b024
benchmarkco <benchmark.co.2022@hmail.com>
2023-01-06 00:46:46 -0500

    deploy sin nada

--- src/components/TopContributors/TableContributors.jsx
+++ src/components/TopContributors/TableContributors.jsx
@@ -14,19 +14,19 @@ const TableContributors = () => {
             <tbody>
             <Contributor
                 redirect_to='/front/profile'
-                img_link="https://media.licdn.com/dms/image/C4D03AQHe2SopuFt7cA/profile-displayphoto-shrink_400_400/0/1526669296827?e=1678320000&v=beta&t=cJANuJ19ODfTv_8tycZVBLLWP16PVb5BjH6MWyVXgu4" 
+                img_link="" 
                 name="Andres Ferro"
                 linkedin_link= "https://linkedin.com/andresferro6"
             />
             <Contributor 
                 redirect_to='/'
-                img_link="https://media.licdn.com/dms/image/C5603AQE6DeYbj6bhvQ/profile-displayphoto-shrink_400_400/0/1620859243757?e=1678320000&v=beta&t=VhntovjVmoPciZTsxoQYrT1j1L-atC3P8fNPKNvYaNA"
+                img_link=""
                 name="Santiago Tamayo"
                 linkedin_link= "https://www.linkedin.com/in/santiago-tamayo-torres-480028114/"
             />
             <Contributor
                 redirect_to='/'
-                img_link="https://media.licdn.com/dms/image/D4E03AQFKxh3F8YZtGg/profile-displayphoto-shrink_400_400/0/1670356478221?e=1676505600&v=beta&t=HoFJHVtw7emT1KPYFPQ7fQWKgtMNmWN6MLzm4ueLrxA"
+                img_link=""
                 name="Alejandro Gallo"
                 linkedin_link="https://www.linkedin.com/in/alejandrogallovalencia"
             />
